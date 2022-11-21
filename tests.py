2f8697abfe1a58ffceacee3146523fbd3254fa61
gusta <tofolo17@gmail.com>
2022-11-21 17:07:13 -0300

    Commit 02: correção passeio aleatório.

--- tests.py
+++ tests.py
@@ -8,21 +8,23 @@ user32 = ctypes.windll.user32
 w, h = user32.GetSystemMetrics(0) // 2, user32.GetSystemMetrics(1) // 2
 nw, nh = w, h
 
-v = 1
+v = 5
 
 while True:
     if keyboard.is_pressed("q"):
         break
 
-    if abs(nw - w) > 300 or abs(nh - h) > 300:
+    if abs(nw - w) > 300 or abs(nh - h) > 150:
         nw, nh = w, h
 
     r = randint(1, 4)
     if r == 1:
-        pyautogui.moveTo(nw + v, nh)
-    if r == 1:
-        pyautogui.moveTo(nw - v, nh)
-    if r == 1:
-        pyautogui.moveTo(nw, h + nh)
-    if r == 1:
-        pyautogui.moveTo(nw, h - nh)
+        nw += v
+    if r == 2:
+        nw -= v
+    if r == 3:
+        nh += v
+    if r == 4:
+        nh -= v
+
+    pyautogui.moveTo(nw, nh)
